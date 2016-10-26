bc3c9f0de30a70886dfe4f958584a996fc3d805e
zhangjikai <zhangjk1993@gmail.com>
2016-10-26 14:07:02 +0800

    update

--- encryption/src/main/java/com/zhangjikai/encrypt/Encryption.java
+++ encryption/src/main/java/com/zhangjikai/encrypt/Encryption.java
@@ -1,6 +1,9 @@
 package com.zhangjikai.encrypt;
 
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 /**
@@ -27,5 +30,21 @@ public class Encryption {
         byte[] bytes = Base64.getDecoder().decode(encyStr);
         original = new String(bytes);
         return original;
+    }
+
+    public static String MD5(String original) {
+        String encyStr = "";
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            byte[] bytes = original.getBytes("UTF-8");
+            md5.update(bytes);
+            BigInteger integer = new BigInteger(md5.digest());
+            encyStr = integer.toString(16);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encyStr;
     }
 }
--- encryption/src/test/java/TestEncryption.java
+++ encryption/src/test/java/TestEncryption.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class TestEncryption {
 
     @Test
-    public void testBase64() {
+     public void testBase64() {
         String s = "\n" +
                 "\n" +
                 "                                                Sulfate-(O-6)+\n" +
@@ -25,6 +25,28 @@ public class TestEncryption {
         String encodeStr = Encryption.base64Encode(s);
         System.out.println(encodeStr);
         System.out.print(Encryption.base64Decode(encodeStr));
+
+    }
+
+    @Test
+    public void testMD5() {
+        String s = "\n" +
+                "\n" +
+                "                                                Sulfate-(O-6)+\n" +
+                "                                                             |\n" +
+                " Sulfate-(O-6)-a-D-Glcp2Me3Me4Me-(1-4)-b-D-GlcpA2Me3Me-(1-4)+|                  Sulfate-(O-6)+\n" +
+                "                                                            ||                               |\n" +
+                "                                                        a-D-Glcp-(1-4)-a-L-IdopA2Me3Me-(1-4)+|\n" +
+                "                                                            ||                              ||\n" +
+                "                                               Sulfate-(O-3)+|                          a-D-Glcp-(1-1)-methyl\n" +
+                "                                                             |                              ||\n" +
+                "                                                Sulfate-(o-2)+                 Sulfate-(O-3)+|\n" +
+                "                                                                                             |\n" +
+                "                                                                                Sulfate-(O-2)+\n";
+
+        String encodeStr = Encryption.MD5(s);
+        System.out.println(encodeStr);
+        //System.out.print(Encryption.base64Decode(encodeStr));
 
     }
 }
